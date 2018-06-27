f1fa6f940dc31a121f7ea3d163d804c4d04de214
Yan Zhuk <yan.zhuk45@gmail.com>
2018-06-27 11:34:27 -0400

    adjust tree hierarchy for online data

--- src/CountryTreeView.js
+++ src/CountryTreeView.js
@@ -76,38 +76,75 @@ const dataSource = [
 // [controlled component](http://facebook.github.io/react/docs/forms.html#controlled-components)
 // is preferred.
 class CountryTreeView extends Component{
+  state = {
+    data: null,
+  }
 
-    componentDidMount () {
+  componentWillMount () {
     axios.get('https://raw.githubusercontent.com/curran/data/gh-pages/un/placeHierarchy/countryHierarchy.json')
     .then((response) => {
-      console.log('response ', response.data)
+      console.log('response ', response.data.children)
+      this.setState({
+        data: response.data.children
+      })
     })
   }
   render() {
-    return (
-      <div>
-        {dataSource.map((node, i) => {
-          const type = node.type;
-          const label = <span className="node">{type}</span>;
+    let onlineData = <p>loading...</p>
+    if (this.state.data) { 
+      onlineData = <div>
+        {this.state.data.map((node, i) => {
+          const continentName = node.data.id;
+          const label = <span className="node">{continentName}</span>;
           return (
-            <TreeView key={type + '|' + i} nodeLabel={label} defaultCollapsed={false}>
-              {node.subregions.map(subregion => {
-                const label2 = <span className="node">{subregion.name} </span>;
+            <TreeView key={i} nodeLabel={label} defaultCollapsed={false}>             
+              {node.children.map(subregion => {
+                const subregionName = subregion.data.id
+                const label2 = <span className="node">{subregionName} </span>;
                 return (
-                  <TreeView  key={subregion.name} nodeLabel={label2} defaultCollapsed={false}>
-                    {subregion.countries.map(country => {
-                      const label3 = <span className="node">{country.name} </span>;
+                  <TreeView key={subregion.data.id} nodeLabel={label2} defaultCollapsed={false}>
+                  {console.log("subregion: ", subregion)}
+                    { (subregion.children) ? subregion.children.map(country => {
+                      const label3 = <span className="node">{country.data.id}</span>;
                       return (
-                        <div className="info">{country.name}</div>
-                        
+                        <div className="info">{label3}</div>
                       )
-                    })}
-                  </TreeView>
-                );
+                    }) : null}
+                    </TreeView>
+                )
               })}
             </TreeView>
-          );
+          
+          )
         })}
+      </div>
+    }
+    let localData = <div>
+      {dataSource.map((node, i) => {
+      const type = node.type;
+      const label = <span className="node">{type}</span>;
+      return (
+        <TreeView key={type + '|' + i} nodeLabel={label} defaultCollapsed={false}>
+          {node.subregions.map(subregion => {
+            const label2 = <span className="node">{subregion.name} </span>;
+            return (
+              <TreeView  key={subregion.name} nodeLabel={label2} defaultCollapsed={false}>
+                {subregion.countries.map(country => {
+                  const label3 = <span className="node">{country.name} </span>;
+                  return (
+                    <div className="info">{country.name}</div>
+                    )
+                  })}
+                </TreeView>
+              );
+            })}
+          </TreeView>
+        );
+      })}
+      </div>
+      return (
+      <div>
+        {onlineData}
       </div>
     );
   }
@@ -116,11 +153,3 @@ class CountryTreeView extends Component{
 export default CountryTreeView;
 
 
-
-// {subregion.map(country => {
-//   const label3 = <span className="node">{country.name}</span>;
-//   return (
-//     <TreeView nodeLabel={label3} key={country.name} defaultCollaped={false}>  
-//     </TreeView>
-//   )
-// })}
